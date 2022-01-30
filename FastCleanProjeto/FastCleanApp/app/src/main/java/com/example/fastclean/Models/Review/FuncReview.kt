831486f2eefc76ec0eab6e05d656ab2b2ffc82d5
Joao Leite <8170556@estg.ipp.pt>
2022-01-30 21:59:39 +0000

    fastclean final

---
+++ FastCleanProjeto/FastCleanApp/app/src/main/java/com/example/fastclean/Models/Review/FuncReview.kt
@@ -0,0 +1,17 @@
+package com.example.fastclean.Models.Review
+
+import com.example.fastclean.Models.Utilizador.Cliente.ClienteRegister
+import com.example.fastclean.Models.Utilizador.Funcionario.FuncionarioRegister
+import com.example.fastclean.Models.Utilizador.Funcionario.FuncionarioUpdate
+import java.io.Serializable
+
+data class FuncReview(
+    val id : Int,
+    val comentario : String,
+    val cotacao : Float,
+    val data : String,
+    val reviewedId: Int,
+    val reviewedName: String,
+    val reviewerId: Int,
+    val reviewerName: String,
+): Serializable
